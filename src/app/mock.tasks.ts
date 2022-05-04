80873816b797cb04f98d1199ca249e2d34d711c6
AmenNkala <amen@bbd.co.za>
2022-05-04 13:30:50 +0200

    initial commit

---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Doctor's Appointement",
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at school',
+    day: 'May 6th at 2:10pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
